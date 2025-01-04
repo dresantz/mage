195f15a1df4e5c0e5af9b24d1c1d25545a8f66e8
Dre Santos <dre.santoz@gmail.com>
2025-01-04 20:21:25 -0300

    :art: Criada a nova barra de vida
    Fiz alterações no código HealthBarUi para usar o valor do Slider no lugar do antigo FillAmount

--- Assets/Scripts/Health/HealthBarUI.cs
+++ Assets/Scripts/Health/HealthBarUI.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class HealthBarUI : MonoBehaviour
 {
     [SerializeField]
-    private UnityEngine.UI.Image healthBarForegroundImage;
-
+    private UnityEngine.UI.Slider healthBarForeground;
 
     // Esse parâmetro healthController aparece no evento de HealthChange justamente por ser um parâmetro
     public void UpdateHealthBar(HealthController healthController)
     {
-        // fillAmount preenche a barra com a quantidade equivalente a porcentagem de vida
-        // determinada no código do HealthController.
-        healthBarForegroundImage.fillAmount = healthController.remainingHeatlthPercentage;
+        // value preenche a barra com a quantidade equivalente a porcentagem de vida
+        healthBarForeground.value = healthController.remainingHeatlthPercentage;
+        // remainingHeatlthPercentage é determinada no código do HealthController.
     }
 }
