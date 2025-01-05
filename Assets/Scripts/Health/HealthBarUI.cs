b863d4f65405de8216b73a32688e434f62ec5c6c
Dre Santos <dre.santoz@gmail.com>
2025-01-05 12:27:29 -0300

    :bug: Ajustado o código HealthBarUI para substituir o Slider que estava dando conflito com a Mask durante o Play Mode

--- Assets/Scripts/Health/HealthBarUI.cs
+++ Assets/Scripts/Health/HealthBarUI.cs
@@ -1,17 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthBarUI : MonoBehaviour
 {
     [SerializeField]
-    private UnityEngine.UI.Slider healthBarForeground;
+    private RectTransform fillRect; // Referência ao objeto Fill
 
-    // Esse parâmetro healthController aparece no evento de HealthChange justamente por ser um parâmetro
     public void UpdateHealthBar(HealthController healthController)
     {
-        // value preenche a barra com a quantidade equivalente a porcentagem de vida
-        healthBarForeground.value = healthController.remainingHeatlthPercentage;
-        // remainingHeatlthPercentage é determinada no código do HealthController.
+        if (fillRect != null)
+        {
+            // Usa o valor normalizado de remainingHealthPercentage para ajustar a escala vertical (Y)
+            fillRect.localScale = new Vector3(1f, healthController.remainingHeatlthPercentage, 1f);
+        }
     }
 }
